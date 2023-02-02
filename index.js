ab441778cb5a2262069c9269df6f48d2959f1956
bloocat <82405323+bloocat@users.noreply.github.com>
2023-02-02 00:47:26 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var iframe = document.getElementById("ifr");
+var elmnt = iframe.contentWindow//.document.body;
+
 function myFunction() {
     var iframe = document.getElementById("ifr");
     var elmnt = iframe.contentWindow//.document.body;
@@ -8,3 +11,41 @@ function myFunction() {
       alert(e)
   }
   }
+
+  //console
+  const consoleInput = document.querySelector(".console-input");
+const historyContainer = document.querySelector(".console-history");
+
+function addResult(inputAsString, output) {
+  const outputAsString =
+    output instanceof Array ? `[${output.join(", ")}]` : output.toString();
+  const inputLogElement = document.createElement("div");
+  const outputLogElement = document.createElement("div");
+
+  inputLogElement.classList.add("console-input-log");
+  outputLogElement.classList.add("console-output-log");
+
+  inputLogElement.textContent = `> ${inputAsString}`;
+  outputLogElement.textContent = outputAsString;
+
+  historyContainer.append(inputLogElement, outputLogElement);
+}
+
+consoleInput.addEventListener("keyup", (e) => {
+  const code = consoleInput.value.trim();
+
+  if (code.length === 0) {
+    return;
+  }
+
+  if (e.key === "Enter") {
+    try {
+      addResult(code, elmnt.eval(code));
+    } catch (err) {
+      addResult(code, err);
+    }
+
+    consoleInput.value = "";
+    historyContainer.scrollTop = historyContainer.scrollHeight;
+  }
+});
